a1f55be036b946c3d683d08f50c79ada205c8555
unknown <fahim.chowdhury@.omega.wwads.net>
2013-01-16 11:26:27 +0000

    added files

---
+++ SimpleTweener.js
@@ -0,0 +1,70 @@
+var tween = function(obj, duration, props, callback) {
+				var framerate = 35;
+				var par = this;
+				var handler= function(){par.onFrame(obj, duration, props, callback);};
+				
+				
+				window.tweenTimer = setInterval(handler,35);
+				
+				this.onFrame = function(obj, duration, props, callback)
+				{
+					for (var name in props) {	
+						var newProp;
+						
+						if(props[name].value==null)
+						{
+							var suffix = (String(props[name]).indexOf("px")>=0);
+							newProp = String(props[name]).replace("px", "");
+							props[name]={value:newProp,negative:null,complete:false,subtraction:null,currentProp:null,suffix:suffix};
+							props[name].currentProp = obj.style[name].replace("px", "");
+						
+						}else{
+							newProp = props[name].value;
+						}
+							
+						if ( props[name].currentProp == null || props[name].currentProp == "")
+						{
+							if(name=="opacity")
+							{
+								props[name].currentProp = 1;
+							}else{
+								props[name].currentProp = 0;
+							}
+						}
+									
+						
+						if(props[name].negative==null)
+							{
+								props[name].negative = Number(newProp) < Number(props[name].currentProp)  ? true : false;
+							
+								props[name].subtraction = Math.abs(Number(props[name].currentProp) - Number(newProp)) / framerate;
+							}
+						
+						if (props[name].negative && Number(props[name].currentProp) >= Number(newProp) || !props[name].negative && Number(props[name].currentProp) <= Number(newProp)) {
+							
+							
+							if(props[name].negative)
+							{
+								props[name].currentProp = Number(props[name].currentProp) - Number(props[name].subtraction); 
+								
+							}else{
+								
+								props[name].currentProp = Number(props[name].currentProp) + Number(props[name].subtraction); 
+							}
+							
+							obj.style[name] = props[name].currentProp + (props[name].suffix?"px":"");
+							if(name=="opacity")this.setOpacity(obj,props[name].currentProp);
+						}else{
+							window.clearInterval(window.tweenTimer);
+						}
+	
+					}
+				}
+				this.setOpacity=function(obj,value)
+				{
+					obj.style['-ms-filter']="progid:DXImageTransform.Microsoft.Alpha(Opacity="+(value*100)+")";
+					obj.style['filter']=value;
+					obj.style['-moz-opacity']=value;
+					obj.style['-khtml-opacity']=value;
+				}
+			}
